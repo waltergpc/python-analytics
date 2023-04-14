77d0577ae38c24b9a2b30127be6993cd2799b7d7
Walter González-Plata Cortes <waltergpc@hotmail.com>
2023-04-13 20:01:43 -0600

    extract data from player stats

--- scrapy.py
+++ scrapy.py
@@ -17,20 +17,19 @@ tbody_list = soup_player_stats.select(
 tr_list = tbody_list[0].select("tr")
 
 for players in tr_list:
-    td_lst = players.select("td")
-    player_params = td_lst[0].a["href"]
+    player_params = players.select("td")[0].a["href"]
     temp_player_stats = requests.get(base_url + player_params)
     temp_soup_stats = bs(temp_player_stats.text, "html.parser")
-    print(temp_soup_stats.select(".nfl-c-player-info__physical-data"))
+    print(temp_soup_stats.select(".nfl-c-player-header"))
 
-    print(
-        players.select("td")[0].a.string,
-        list(
-            map(
-                lambda x: x.string,
-                temp_soup_stats.select(
-                    "#main-content > section:nth-child(5) > div > div.d3-l-col__col-8 > section > div > div > div > section > div > div.d3-l-col__col-12.nfl-c-player-info__content"
-                ),
-            )
-        ),
-    )
+    # print(
+    #     players.select("td")[0].a.string,
+    #     list(
+    #         map(
+    #             lambda x: x.string,
+    #             temp_soup_stats.select(
+    #                 "#main-content > section:nth-child(5) > div > div.d3-l-col__col-8 > section > div > div > div > section > div > div.d3-l-col__col-12.nfl-c-player-info__content"
+    #             ),
+    #         )
+    #     ),
+    # )
