c0089ec5d44624ac9410b3f4765c136ffda6d2fc
Walter González-Plata Cortes <waltergpc@hotmail.com>
2023-05-01 19:30:27 -0600

    introduce baku race

--- main.py
+++ main.py
@@ -9,27 +9,27 @@ import pandas as pd
 
 ff1.Cache.enable_cache("cache")
 
-bahrain_r_2023 = ff1.get_session(2022, "Austria", "R")
+baku_r_2023 = ff1.get_session(2023, "Baku", "R")
 
-bahrain_r_2023.load()
+baku_r_2023.load()
 
-driver_1, driver_2 = "PER", "LEC"
+driver_1 = "PER"
 
-laps_driver_1 = bahrain_r_2023.laps.pick_driver(driver_1)
-laps_driver_2 = bahrain_r_2023.laps.pick_driver(driver_2)
+laps_driver_1 = baku_r_2023.laps.pick_driver(driver_1)
+
+print(laps_driver_1)
 
 fastest_driver_1 = laps_driver_1.pick_fastest()
-fastest_driver_2 = laps_driver_2.pick_fastest()
+
+print(fastest_driver_1)
+
 
 telemetry_driver_1 = fastest_driver_1.get_telemetry().add_distance()
-telemetry_driver_2 = fastest_driver_2.get_telemetry().add_distance()
 
 team_driver_1 = fastest_driver_1["Team"]
-team_driver_2 = fastest_driver_2["Team"]
 
-delta_time, ref_tel, compare_tel = utils.delta_time(fastest_driver_1, fastest_driver_2)
 
-plot_size = [15, 15]
-plot_title = f"{bahrain_r_2023.event.year} {bahrain_r_2023.event.EventName} - {bahrain_r_2023.name} - {driver_1} VS {driver_2}"
-plot_ratios = [1, 3, 2, 1, 1, 2, 1]
-plot_filename = plot_title.replace(" ", "") + ".png"
+# plot_size = [15, 15]
+# plot_title = f"{bahrain_r_2023.event.year} {bahrain_r_2023.event.EventName} - {bahrain_r_2023.name} - {driver_1} VS {driver_2}"
+# plot_ratios = [1, 3, 2, 1, 1, 2, 1]
+# plot_filename = plot_title.replace(" ", "") + ".png"
